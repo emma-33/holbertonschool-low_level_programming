7e13290a2f24be88e2ce91106ac795b27e3ecb2f
emma-33 <emma.lateyron@orange.fr>
2023-10-11 01:33:43 -0700

    4 with betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+/**
+ *main - prints exactly "Programming is like building a multilingual puzzle,
+ *followed by a new line
+ *argc - integer variable that stores the number of command-line arguments
+ *passed by the user including the name of the program
+ *argv - array of character pointers listing all the arguments
+ */
 
-  char str1[] = "\"Programming is like building a multilingual puzzle";
-
-  puts(str1);
-
-  return 0;
+int main(int argc, char *argv[])
+{
+char str1[] = "\"Programming is like building a multilingual puzzle";
+puts(str1);
+return (0);
 }
 
 
