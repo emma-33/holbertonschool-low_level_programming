2c04cf10c3c4c6681bf5504a484166ff02c7b849
emma-33 <emma.lateyron@orange.fr>
2023-10-11 01:44:14 -0700

    4 with betty doc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,12 +4,11 @@
 /**
  *main - prints exactly "Programming is like building a multilingual puzzle,
  *followed by a new line
- *argc - integer variable that stores the number of command-line arguments
- *passed by the user including the name of the program
- *argv - array of character pointers listing all the arguments
+ *
+ *Return:  0 on success. Error code otherwise
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
 char str1[] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
