dc6af13737037f100a1681c5c3931f72cfcf1e9f
emma-33 <emma.lateyron@orange.fr>
2023-10-12 06:51:19 -0700

    101 without space

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 puts(str1);
 return (1);
 }
