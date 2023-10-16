1b67c6caf3f60fe96d6a68da5282cd186319eb09
emma-33 <emma.lateyron@orange.fr>
2023-10-16 08:05:27 -0700

    1 with betty style

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <time.h>
 /**
- * betty style doc for function main goes there 
+ * main - assign a random number to the variable n and
+ * prints if the last number of nthe last number of n is greater than 5,
+ * equals to 0 or less than 6
+ * Return: Prints if the last number of n is greater than 5,
+ * equals to 0 or less than 6 and not equal to 6
  */
 int main(void)
 {
@@ -10,19 +14,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	scanf("%d",&n);
+	scanf("%d", &n);
 	last_digit = n % 10;
-	if( last_digit > 5 )
+	if (last_digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
 	}
-	else if( last_digit == 0 )
+	else if (last_digit == 0)
 	{
 		printf("Last digit of %d is %d and is 0", n, last_digit);
 	}
-	else if( last_digit < 6 && last_digit != 0 )
+	else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
-	} 
+	}
 	return (0);
 }
