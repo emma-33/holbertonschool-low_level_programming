aa8c56d9b78893cbaae0ab2d57f51d886cb65833
emma-33 <emma.lateyron@orange.fr>
2023-10-18 01:21:31 -0700

    3-is lower and readme

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point,checks for lowercase
+ * character
+ * @c: character to check
+ *
+ * Return: 1 if c is lowercase else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+
+}
