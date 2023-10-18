3d988d8adb6bb109571371718e0e22a8a9edf05a
emma-33 <emma.lateyron@orange.fr>
2023-10-18 13:36:54 -0700

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+
+}
