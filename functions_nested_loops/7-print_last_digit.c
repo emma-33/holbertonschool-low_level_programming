81c1b7bf7c9d5359640f4734095ac08e58bc3aca
emma-33 <emma.lateyron@orange.fr>
2023-10-18 05:32:31 -0700

    7 with if

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,27 +2,20 @@
 
 /**
  * print_last_digit - Entry point, prints the last digit of * n
+ *
  * @n: int to print the last digit of
- * @last_digit:last digit of n
  * Return: value of last digit
  */
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last_digit = n % 10;
 
-	if (n > 0)
+	if (last_digit < 0)
 	{
-		last_digit = n % 10;
-		_putchar('0' + last_digit);
+		last_digit *= -1;
 	}
-
-	else
-	{
-		n = -n;
-		last_digit = n % 10;
-		_putchar('0' + last_digit);
-	}
+	_putchar('0' + last_digit);
 
 	return (last_digit);
 
