5dc71681294122719ffa9f8bded03f391c5b1c33
emma-33 <emma.lateyron@orange.fr>
2023-10-19 09:38:59 -0700

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point, multiplies a and b
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+	return (mul);
+}
