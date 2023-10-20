2f0d49504446066e5dd98118416afcba6c63c59e
emma-33 <emma.lateyron@orange.fr>
2023-10-20 06:11:53 -0700

    100

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -2,37 +2,35 @@
 #include "main.h"
 
 /**
- * prime_factors - find and print the prime factors of n
+ * main - find and print the prime factors of n
  *
- * @n: long int to test
- * @i: int to divided by
+ * Return: 0
  */
 
 int main(void)
 {
-	int i;
+	long int i;
 	long int n = 612852475143;
-	
-	while (n%2 == 0)
+	long int max;
+
+	while (n % 2 == 0)
 	{
-		printf("%d ", 2);
-		n = n/2;
+		max = 2;
+		n = n / 2;
 	}
 
-	for (i = 3; i * i <= n; i = i+2)
+	for (i = 3; i * i <= n; i = i + 2)
 	{
-		while (n%i == 0)
+		while (n % i == 0)
 		{
-			printf("%d ", i);
-			n = n/i;
+			max = i;
+			n = n / i;
 		}
 	}
 	if (n > 2)
 	{
-		printf("%ld ", n);
+		max = n;
 	}
-	printf("\n");
+	printf("%ld\n", max);
 	return (0);
-
 }
-	
