2ca9ba173adf61d5351f5b67102bd858cd746e78
emma-33 <emma.lateyron@orange.fr>
2023-11-01 14:02:44 -0700

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,20 +14,18 @@ int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
-	int mul = 0;
+	int result = 0;
 
-	if (argc == 3)
+	if (argc < 3 || argc > 3)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d\n", mul);
-
-	}
-	else
-	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
 	return (0);
 }
