b27f25cffdf696aeb6d8995d8acb4fdd9e1fb549
emma-33 <emma.lateyron@orange.fr>
2023-11-06 04:23:08 -0800

    strconcat and readme

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * 
+ * Return: pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated or NULL if failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+
+	while (s1 && s1[len1])
+	{
+		len1++;
+	}
+	while (s2 && s2[len2])
+	{
+		len2++;
+	}
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (s3 == NULL)
+	{
+		return(NULL);
+	}
+	
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] =	s1[i];
+			i++;
+		}
+	}
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+	return (s3);
+}
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -1,2 +1,3 @@
 Function 0 : creates an array of chars, and initializes it with a specific char
 Function 1 : returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter
+Function 2 : concatenates two strings
