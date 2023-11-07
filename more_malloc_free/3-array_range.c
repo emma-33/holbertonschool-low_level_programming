0b5a3f79cef03e3ccb27094b6bb1df2819470bac
emma-33 <emma.lateyron@orange.fr>
2023-11-07 02:29:34 -0800

    3 array with betty readme and valgrind

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ *
+ * @min: minimum value
+ * @max: maximum value
+ *
+ * Return: pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int i, j = 0;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(int) * (max - min + 1));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = min; i <= max; i++)
+	{
+		array[j] = i;
+		j++;
+	}
+	return (array);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -1,3 +1,4 @@
 Function 0 : allocates memory with malloc
 Function 1 : concatenates two strings
 Function 2 : allocates memory for an array, using malloc
+Function 3 : creates an array of integers
