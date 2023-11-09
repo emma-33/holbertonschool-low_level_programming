3e843cac3ab0b16922764d9823821743a9871594
emma-33 <emma.lateyron@orange.fr>
2023-11-09 13:49:14 -0800

    100 with betty

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combination of two digits
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i != '8' || j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
