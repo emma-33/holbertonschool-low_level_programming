236ea84c3d45e1b93282171ac8b2961df3a2bb58
emma-33 <emma.lateyron@orange.fr>
2023-11-13 06:45:59 -0800

    1 array betty readme

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator -  executes a function given as a parameter
+ * on each element of an array
+ *
+ * @array: array to work with
+ * @size: size of array
+ * @action: pointer to the fonction you need to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- function_pointers/README.md
+++ function_pointers/README.md
@@ -1,1 +1,2 @@
-Function 1 : prints a name
+Function 0 : prints a name
+Function 1 : executes a function given as a parameter on each element of an array
