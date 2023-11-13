a547a9035e2b89fb73b64204be024c938321995e
emma-33 <emma.lateyron@orange.fr>
2023-11-13 03:08:12 -0800

    5 free betty valgrind readme

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ *
+ * @d: pointer to dog
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
