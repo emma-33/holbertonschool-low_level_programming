d8e4316083a884c43b4401373b6f03650d848a52
emma-33 <emma.lateyron@orange.fr>
2023-11-14 05:28:43 -0800

    1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdargs.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ *
+ * @separator: string to be printed between numbers
+ * @n: number of int passed in the function
+ *
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator == NULL)
+	{
+		/* don't print it */
+	}
+}
