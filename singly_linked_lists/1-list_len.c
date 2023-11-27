8a8a6bb1840b743e2153bc018725d99209c7730d
emma-33 <emma.lateyron@orange.fr>
2023-11-27 14:02:02 +0100

    fix 1 list len and betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements of a linked list
- * @h: pointer to the list
- * Return: number of elements of the list
- */
+* list_len - returns the number of elements of a linked list
+* @h: pointer to the list
+* Return: number of elements of the list
+*/
 size_t list_len(const list_t *h)
-
 {
-int count = 0;
-current = h;
-while (current != NULL)
-{
-current = current->next;
-count++;
-}
-return (count);
+	const list_t *current;
+	int count = 0;
 
+	current = h;
+
+	while (current != NULL)
+	{
+		if (current->str != NULL || current->len > 0)
+		{
+			count++;
+		}
+		current = current->next;
+	}
+	return (count);
 }
---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/*
+* add_node - adds a new node at the beginning of a list_t list
+* @head: list
+* @str: string to duplicate
+* Return: address of the new element or NULL if it fails
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+    
+}
