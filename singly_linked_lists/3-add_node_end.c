dd4edeb71e44269c14e4a299003dd7f93e460a3a
emma-33 <emma.lateyron@orange.fr>
2023-11-27 14:55:30 +0100

    add 3 node end and betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 
 /**
-* add_node - adds a new node at the beginning of the list
+* add_node_end - adds a new node at the end of the list
 * @head: head of the list
 * @str: string to duplicate
 * Return: address of the new element or NULL if it fails
 */
-
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *ptr;
 
 	new_node = malloc(sizeof(list_t));
+	ptr = *head;
 
 	if (new_node == NULL)
 	{
@@ -25,8 +26,16 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->len = strlen(new_node->str);
-	new_node->next = *head;
-	*head = new_node;
-
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = new_node;
 	return (new_node);
 }
