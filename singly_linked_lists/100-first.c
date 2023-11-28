0b4fd3fe9e9dc9477453299d09bcf816481e0805
emma-33 <emma.lateyron@orange.fr>
2023-11-28 18:42:05 +0100

    add 100 first

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void before_main(void) __attribute__((constructor));
+/**
+* before_main - prints stuff before main
+*/
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
