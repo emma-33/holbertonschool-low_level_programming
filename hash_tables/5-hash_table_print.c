dfe82ea4802e9716d59b0977a8ebf30b7a0bd208
emma-33 <emma.lateyron@orange.fr>
2023-12-05 18:15:29 +0100

    add 5 print and readme

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - prints a hash table
+* @ht: hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+
+	int flag = 0;
+
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+
+				if (node != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
--- hash_tables/README.md
+++ hash_tables/README.md
@@ -1,1 +1,5 @@
 Function 1 : creates a hash table
+Function 2 : gives the index of a key
+Function 3 : adds an element to the hash table
+Function 4 : retrieves the value associated with a key
+Function 5 : prints the hash table
