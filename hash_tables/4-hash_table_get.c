b5dcb2b7ddb7bbfc17f16616ddbd391c4ca75ec1
emma-33 <emma.lateyron@orange.fr>
2023-12-05 16:10:00 +0100

    add 4 get

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - retrieves a value associated with a key
+* @ht: hash table to look into
+* @key: key you are looking for
+* Return: value associated with the element or NULL
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+
+	hash_node_t *tmp;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	i = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
