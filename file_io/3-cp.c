62043bcd5bc4c0a8f4d11fa6eef233efdb93dbd8
emma-33 <emma.lateyron@orange.fr>
2023-12-06 14:53:39 +0100

    test cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
-* create_buffer - creates a buffer
-* @file: name of file buffer is storing chars for
-* Return: pointer to newly allocated buffer
-*/
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		exit(99);
-	}
-	return (buffer);
-}
-
-/**
 * close_file - closes files
 * @file: file to be closed
 */
@@ -40,14 +21,13 @@ void close_file(int file)
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
-	char *buffer;
+	char *buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
@@ -57,20 +37,17 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
 			exit(98);
 		}
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
 			exit(99);
 		}
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 	}
-	free(buffer);
 	close_file(from);
 	close_file(to);
 
