89fda91308a88b62b48d312dc4cfefdb2fe0ceeb
emma-33 <emma.lateyron@orange.fr>
2023-12-06 11:55:59 +0100

    fix 3 cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 /**
+* create_buffer - creates a buffer
+* @file: name of file buffer is storing chars for
+* Return: pointer to newly allocated buffer
+*/
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+* close_file - closes files
+* @file: file to be closed
+*/
+void close_file(int file)
+{
+	if (close(file) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", file);
+		exit(100);
+	}
+}
+/**
 * main - Copies the content of a file into another file
 * @argc: number of arguments
 * @argv: agrument vector
@@ -8,43 +39,40 @@
 */
 int main(int argc, char *argv[])
 {
-	int from, to, r, close_f, close_t;
-
-	char buff[1024];
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	if (from == -1)
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+
+	while (r > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((r = read(from, buff, 1024)) > 0)
-		if (to == -1 || (write(to, buff, r) != r))
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(to);
+			free(buffer);
 			exit(99);
 		}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 	}
-	if (close(from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
-	if (close(to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
 	return (0);
 }
