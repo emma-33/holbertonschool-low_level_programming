efa598c031f663c8aa12858063455cd261284d5e
emma-33 <emma.lateyron@orange.fr>
2024-01-18 15:29:17 +0100

    fix 0-linear.c : remove printf in double

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -16,12 +16,10 @@ int linear_search(int *array, size_t size, int value)
 
 	while (i < (int)size)
 	{
-		if (array[i] == value)
-		{
-			printf("Value checked array [%d] = [%d]\n", i, array[i]);
-			return (i);
-		}
 		printf("Value checked array [%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
 		i++;
 	}
 	return (-1);
