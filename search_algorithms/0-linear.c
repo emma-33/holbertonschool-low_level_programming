b4177de40f82fe40b991a92f61055790926c5346
emma-33 <emma.lateyron@orange.fr>
2024-01-19 10:50:10 +0100

    fix 0-linear.c size_t instead of int and space removed in printf statement

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,14 +9,14 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i = 0;
 
 	if (!array)
 		return (-1);
 
-	while (i < (int)size)
+	while (i < size)
 	{
-		printf("Value checked array [%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
 			return (i);
